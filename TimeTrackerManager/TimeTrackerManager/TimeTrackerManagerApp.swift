672a3ff05e08fade69636990861af03f0153c939
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-10-11 15:23:18 +0300

    create new iPad project TimeTrackerManager

---
+++ TimeTrackerManager/TimeTrackerManager/TimeTrackerManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerManagerApp.swift
+//  TimeTrackerManager
+//
+//  Created by Sebastian Vidrea on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
