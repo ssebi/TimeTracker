19f71301e9b4c86508375bd2ca8d6f2077f526df
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-10-11 15:02:02 +0300

    create iOS TimeTracker client project

---
+++ TimeTracker/TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Sebastian Vidrea on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
