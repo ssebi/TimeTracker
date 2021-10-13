ad0624d3a9236f19f0905af98c4471ee9776ff4c
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-10-13 14:27:44 +0300

    wrap LoginView elements inside a VStack
    * this solves the multiple previews problem

--- TimeTrackerClient/TimeTrackerClient/LoginView.swift
+++ TimeTrackerClient/TimeTrackerClient/LoginView.swift
@@ -13,40 +13,42 @@ struct LoginView: View {
     @State var password: String = ""
 
     var body: some View {
-        Image("timeTrackerIcon")
-            .resizable()
-            .frame(width: 200, height: 230, alignment: .center)
-
-        Text("Time Tracker")
-            .padding()
-            .font(.title)
-            .padding(.bottom, 40)
-            .foregroundColor(Color.cBlack)
-
-        Spacer()
-
         VStack {
-            Group {
-                TextField("E-mail", text: $username)
-                    .padding()
-                    .background(Color.cGray)
-                    .cornerRadius(5.0)
+            Image("timeTrackerIcon")
+                .resizable()
+                .frame(width: 200, height: 230, alignment: .center)
 
-                SecureField("Password", text: $password)
-                    .padding()
-                    .background(Color.cGray)
-                    .cornerRadius(5.0)
-            }
-            .padding(EdgeInsets(top: 10, leading: 25, bottom: 10, trailing: 25))
+            Text("Time Tracker")
+                .padding()
+                .font(.title)
+                .padding(.bottom, 40)
+                .foregroundColor(Color.cBlack)
 
             Spacer()
 
-            Button("Login", action: { })
-                .foregroundColor(.white)
-                .frame(width: UIScreen.main.bounds.width - 45, height: 50, alignment: .center)
-                .background(Color.cGreen)
-                .cornerRadius(5)
-                .padding(.bottom, 50)
+            VStack {
+                Group {
+                    TextField("E-mail", text: $username)
+                        .padding()
+                        .background(Color.cGray)
+                        .cornerRadius(5.0)
+
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color.cGray)
+                        .cornerRadius(5.0)
+                }
+                .padding(EdgeInsets(top: 10, leading: 25, bottom: 10, trailing: 25))
+
+                Spacer()
+
+                Button("Login", action: { })
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 45, height: 50, alignment: .center)
+                    .background(Color.cGreen)
+                    .cornerRadius(5)
+                    .padding(.bottom, 50)
+            }
         }
     }
 
