e232a57139c07869ac99d0d5d43a3eaed806c986
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-10-13 19:12:41 +0300

    Added AppDelegate to silence Firebase error on runtime

--- TimeTrackerClient/TimeTrackerClient/TimeTrackerClientApp.swift
+++ TimeTrackerClient/TimeTrackerClient/TimeTrackerClientApp.swift
@@ -11,13 +11,20 @@ import Firebase
 @main
 struct TimeTrackerClientApp: App {
     
-    init(){
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    class AppDelegate: NSObject, UIApplicationDelegate {
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            
+            FirebaseApp.configure()
+            
+            return true
         }
     }
 }
