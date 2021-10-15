33e9de4168e61481cbf3c16ef49dc278c2cbde9b
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-10-15 12:33:55 +0300

    test sign in is successful on multiple calls

--- TimeTrackerClient/TimeTrackerClientTests/TimeTrackerClientTests.swift
+++ TimeTrackerClient/TimeTrackerClientTests/TimeTrackerClientTests.swift
@@ -10,17 +10,19 @@ import Firebase
 @testable import TimeTrackerClient
 
 class TimeTrackerClientTests: XCTestCase {
-    
-    func testFirebaseFailsWithWrongCredentials() throws {
+
+    /// sut = system under test
+
+    func test_signIn_failsWithInvalidCredentials() throws {
         /// Given
+        let sut = SessionStore()
         let email: String = "mihai24vic"
         let password: String = "123Password"
-        let exp = expectation(description: "Waiting to complete")
         var sesionStoreResult: Result<User, Error>? = nil
-        
+
         /// When
-        let session = SessionStore()
-        session.singIn(email: email, password: password) { result in
+        let exp = expectation(description: "Waiting to complete")
+        sut.singIn(email: email, password: password) { result in
             sesionStoreResult = result
             exp.fulfill()
         }
@@ -35,21 +37,49 @@ class TimeTrackerClientTests: XCTestCase {
         }
     }
     
-    func testFirebaseAuthSuscces() throws {
+    func test_signIn_isSuccessfulOnSingleFunctionCall() throws {
         /// Given
+        let sut = SessionStore()
         let email: String = "mihai24vic@gmail.com"
         let password: String = "Patratel1"
-        let exp = expectation(description: "Waiting to complete")
         var sesionStoreResult: Result<User, Error>? = nil
-        
+
         /// When
-        let session = SessionStore()
-        session.singIn(email: email, password: password) { result in
+        let exp = expectation(description: "Waiting to complete")
+        sut.singIn(email: email, password: password) { result in
             sesionStoreResult = result
             exp.fulfill()
         }
         wait(for: [exp], timeout: 5)
         
+        /// Then
+        XCTAssertNotNil(sesionStoreResult)
+        if case let .success(user) = sesionStoreResult {
+            XCTAssertEqual(user.email, email)
+        } else {
+            XCTFail()
+        }
+    }
+
+    func test_signIn_isSuccessfulOnMultipleFunctionCalls() throws {
+        /// Given
+        let sut = SessionStore()
+        let email: String = "mihai24vic@gmail.com"
+        let password: String = "Patratel1"
+        var sesionStoreResult: Result<User, Error>? = nil
+
+        /// When
+        let exp = expectation(description: "Waiting to complete")
+        exp.expectedFulfillmentCount = 3
+        for _ in 0..<3 {
+            sut.singIn(email: email, password: password) { result in
+                sesionStoreResult = result
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 5)
+
         /// Then
         XCTAssertNotNil(sesionStoreResult)
         if case let .success(user) = sesionStoreResult {
