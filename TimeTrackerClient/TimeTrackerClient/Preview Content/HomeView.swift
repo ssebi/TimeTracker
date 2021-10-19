5d01719e74eb038bc07d6c8c14070d8203f656e3
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-10-19 16:33:10 +0300

    Add HomeView

---
+++ TimeTrackerClient/TimeTrackerClient/Preview Content/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TimeTrackerClient
+//
+//  Created by Bocanu Mihai on 18.10.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
