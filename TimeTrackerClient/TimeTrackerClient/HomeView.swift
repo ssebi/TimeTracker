128aebe80d8366a6e9435d09df189544782b4c11
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-10-20 12:07:34 +0300

    Added empty  Home View

---
+++ TimeTrackerClient/TimeTrackerClient/HomeView.swift
@@ -0,0 +1,38 @@
+//
+//  HomeView.swift
+//  TimeTrackerClient
+//
+//  Created by Bocanu Mihai on 18.10.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack{
+            HStack(){
+                Text("Today")
+                    .padding()
+                    .frame(alignment: .leading)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            HStack{
+                Button("+"){}
+                .buttonStyle(AddButton())
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
