ffdb0ef8fdc39e1e4499ff0cd10c97d473397c10
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-10-22 10:08:24 +0300

    Add client prop to user model

--- TimeTrackerClient/TimeTrackerClient/Models/UserModel.swift
+++ TimeTrackerClient/TimeTrackerClient/Models/UserModel.swift
@@ -11,11 +11,12 @@ class User: ObservableObject {
     @Published var uid: String?
     @Published var email: String?
     @Published var username: String?
+    @Published var client: String?
     
-    init(uid: String?, email: String?, username: String?) {
+    init(uid: String?, email: String?, username: String?, client: String?) {
         self.uid = uid
         self.email = email
         self.username = username
-        
+        self.client = client
     }
 }
