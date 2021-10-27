be50e97a5591092917f98e56e80f4c4794b890c9
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-10-27 23:53:10 +0300

    Populate params for save timeSlots

--- TimeTrackerClient/TimeTrackerClient/AddView.swift
+++ TimeTrackerClient/TimeTrackerClient/AddView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @EnvironmentObject var session: SessionStore
+    
     let clients = ["Client 1","Cient 2","Client 3","Client 4"]
     let projects = ["Project 1","Project 2","Project 3","Project 4"]
     @State private var projectSelection = "Project x"
@@ -59,19 +62,24 @@ struct AddView: View {
     }
     
     func addTime(){
-        print("444444444")
-        let data: [String: Any] = [
-            "date": "20-10-2021",
+        let user = session.session
+        let userId = user?.uid ?? ""
+        var path = ""
+        if user != nil {
+            path = "userId/\(userId)/\(clientSelection)/\(projectSelection)/timelLoged/19-01-2021/timeslots"
+        }
+        
+        print("time log saved")
+        
+        let timeslot: [String: Any] = [
             "timeSlots": [
-                [
-                    "start": "20-10-2021T12:00:00Z",
-                    "end": "20-10-2021T15:00:00Z",
-                    "description": $description
-                ],
-            ],
+                "start": "21-10-2022T12:11:00Z",
+                "end": "21-10-2021T15:10:00Z",
+                "description": description ],
+            "total": 5
         ]
         
-        dataStore.addTimeSlot(with: data, to: projectSelection){ error in }
+        dataStore.addTimeSlot(with: timeslot, to: path){ error in }
     }
 }
 
