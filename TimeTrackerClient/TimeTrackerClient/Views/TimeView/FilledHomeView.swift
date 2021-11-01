24e7d346f71a2cfd2361623cae3b9cdd4edb9548
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-11-01 15:41:07 +0200

    Fix indent

--- TimeTrackerClient/TimeTrackerClient/Views/TimeView/FilledHomeView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/TimeView/FilledHomeView.swift
@@ -12,26 +12,26 @@ struct FilledHomeView: View {
     @EnvironmentObject var session: SessionStore
     
     var body: some View {
-            VStack{
-                ProjectView()
-                    .padding()
-            }
-            .navigationBarItems(
-                  leading: Button(action: {session.singOut()}, label: {
-                      Label("", systemImage: "power")
-                          .foregroundColor(.red)
-                          .font(.system(size: 20))
-                  }),
-                  trailing:
-                        Button(action: {}, label: {
-                          NavigationLink(destination: AddView()) {
-                              Label("+", systemImage: "plus.rectangle.fill")
-                                  .foregroundColor(.cGreen)
-                                  .font(.system(size: 30))
-                         }
-                      })
-                    )
-            .navigationTitle("Time Logged")
+        VStack {
+            ProjectView()
+                .padding()
+        }
+        .navigationBarItems(
+            leading: Button(action: {session.singOut()}, label: {
+                Label("", systemImage: "power")
+                    .foregroundColor(.red)
+                    .font(.system(size: 20))
+            }),
+            trailing:
+                Button(action: {}, label: {
+                    NavigationLink(destination: AddView()) {
+                        Label("+", systemImage: "plus.rectangle.fill")
+                            .foregroundColor(.cGreen)
+                            .font(.system(size: 30))
+                    }
+                })
+        )
+        .navigationTitle("Time Logged")
     }
 }
 
