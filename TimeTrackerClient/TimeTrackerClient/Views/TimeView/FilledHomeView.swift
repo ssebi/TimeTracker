8171d99756115a0255e3180bd006269280571796
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-11-01 15:40:42 +0200

    Add signOut button in navigation bar

--- TimeTrackerClient/TimeTrackerClient/Views/TimeView/FilledHomeView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/TimeView/FilledHomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FilledHomeView: View {
     @State var showView = false
+    @EnvironmentObject var session: SessionStore
     
     var body: some View {
             VStack{
@@ -16,13 +17,20 @@ struct FilledHomeView: View {
                     .padding()
             }
             .navigationBarItems(
-                  trailing: Button(action: {}, label: {
-                      NavigationLink(destination: AddView()) {
-                          Label("+", systemImage: "plus.rectangle.fill")
-                              .foregroundColor(.cGreen)
-                              .font(.system(size: 30))
-                     }
-                  }))
+                  leading: Button(action: {session.singOut()}, label: {
+                      Label("", systemImage: "power")
+                          .foregroundColor(.red)
+                          .font(.system(size: 20))
+                  }),
+                  trailing:
+                        Button(action: {}, label: {
+                          NavigationLink(destination: AddView()) {
+                              Label("+", systemImage: "plus.rectangle.fill")
+                                  .foregroundColor(.cGreen)
+                                  .font(.system(size: 30))
+                         }
+                      })
+                    )
             .navigationTitle("Time Logged")
     }
 }
