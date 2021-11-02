4351f09a5a5550c7eea53cbe6f1963c040dcad20
Mihai Bocanu <mihai@parhelionsoftware.com>
2021-11-02 11:44:56 +0200

    Fix tests errors

--- TimeTrackerClient/TimeTrackerClientTests/DataStoreClientTests.swift
+++ TimeTrackerClient/TimeTrackerClientTests/DataStoreClientTests.swift
@@ -14,7 +14,7 @@ class DataStoreClientTests: XCTestCase {
     
     func test_addTimeSlot_isSusccesfullOnAdd() {
         let sut = makeSUT(withUserSignedIn: true)
-        let slot = TimeSlot(id: UUID(), start: Date.now, end: Date.now + 1, description: "First dscription for log time")
+        let slot = TimeSlotDetail(start: Date.now + 1, end: Date.now + 1, description: "First dscription for log time")
         let exp = expectation(description: "Wait for firebase")
         var receivedError: Error?
         let data: [String: Any] = [
@@ -34,7 +34,7 @@ class DataStoreClientTests: XCTestCase {
     
     func test_addTimeSlot_isNotSusccesfullWithoutUser() {
         let sut = makeSUT(withUserSignedIn: false)
-        let slot = TimeSlot(id: UUID(), start: Date.now, end: Date.now + 1, description: "First dscription for log time")
+        let slot = TimeSlotDetail(start: Date.now, end: Date.now + 1, description: "First dscription for log time")
         let exp = expectation(description: "Wait for firebase")
         let data: [String: Any] = [
             "start": slot.start,
