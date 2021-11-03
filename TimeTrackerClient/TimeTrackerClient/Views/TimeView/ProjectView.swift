5fd48e5d0723efdb3f43077c94c7929b374480bf
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-03 16:45:07 +0200

    simplify ProjectView's List
    - there is no need to use ForEach inside since the `TimeSlot` is `Identifiable`

--- TimeTrackerClient/TimeTrackerClient/Views/TimeView/ProjectView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/TimeView/ProjectView.swift
@@ -8,27 +8,25 @@
 import SwiftUI
 
 struct ProjectView: View {
-    @EnvironmentObject var session: SessionStore
-    @EnvironmentObject var userData: DataStore
-    
-    var body: some View {
-        List {
-            ForEach(userData.userTimeslots, id: \.id){ timeSlot in
-                VStack(alignment: .leading, spacing: 2) {
-                    Text("Project name: Project x")
-                    Text("Date: \(timeSlot.timeSlots.start.formatted(date: .omitted, time: .standard))")
-                    Text("Start time: \(timeSlot.timeSlots.start.formatted(date: .omitted, time: .standard))")
-                    Text("End time: \(timeSlot.timeSlots.end.formatted(date: .omitted, time: .standard))")
-                    Text("Time period: \(timeSlot.total)")
-                    Text("Task description: \(timeSlot.timeSlots.description)")
-                }.padding()
-            }
-        }.listStyle(InsetListStyle())
-    }
+	@EnvironmentObject var userData: DataStore
+
+	var body: some View {
+		List(userData.userTimeslots) { timeSlot in
+			VStack(alignment: .leading, spacing: 2) {
+				Text("Project name: Project x")
+				Text("Date: \(timeSlot.timeSlots.start.formatted(date: .omitted, time: .standard))")
+				Text("Start time: \(timeSlot.timeSlots.start.formatted(date: .omitted, time: .standard))")
+				Text("End time: \(timeSlot.timeSlots.end.formatted(date: .omitted, time: .standard))")
+				Text("Time period: \(timeSlot.total)")
+				Text("Task description: \(timeSlot.timeSlots.description)")
+			}.padding()
+		}.listStyle(InsetListStyle())
+	}
 }
 
 struct Project_Previews: PreviewProvider {
-    static var previews: some View {
-        ProjectView()
-    }
+	static var previews: some View {
+		ProjectView()
+			.environmentObject(DataStore())
+	}
 }
