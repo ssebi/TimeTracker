5c94e85b939b448c0820131e4cef8403d66b0fb5
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-03 16:40:28 +0200

    merge HomeView with FilledHomeView

--- TimeTrackerClient/TimeTrackerClient/Views/TimeView/HomeView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/TimeView/HomeView.swift
@@ -10,14 +10,44 @@ import FirebaseFirestore
 
 struct HomeView: View {
 	@EnvironmentObject var dataStore: DataStore
+	@EnvironmentObject var session: SessionStore
+
+	@State var showView = false
+	private let startEndDate = StartEndDate(start: Date.now, end: Date.now)
 
 	var body: some View {
 		NavigationView {
-			FilledHomeView()
+			ProjectView()
+				.padding()
+				.navigationBarItems(
+					leading:
+						Button(
+							action: {
+								session.singOut()
+							},
+							label: {
+								Label("", systemImage: "power")
+									.foregroundColor(.red)
+									.font(.system(size: 20))
+							}),
+					trailing:
+						Button(
+							action: { },
+							label: {
+								NavigationLink(
+									destination:
+										AddView()
+										.environmentObject(startEndDate)
+								) {
+									Label("+", systemImage: "plus.rectangle.fill")
+										.foregroundColor(.cGreen)
+										.font(.system(size: 30))
+								}
+							})
+				)
+				.navigationTitle("Time Logged")
+				.onAppear(perform: dataStore.fetchUsersTimeslots)
 		}
-		.navigationTitle(
-			Text("Today")
-		)
 	}
 }
 
@@ -25,5 +55,6 @@ struct HomeView_Previews: PreviewProvider {
 	static var previews: some View {
 		HomeView()
 			.environmentObject(DataStore())
+			.environmentObject(SessionStore())
 	}
 }
