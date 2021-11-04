98aee7e1b3987df584bb3557be0aad61bdc4cd02
Mihai Bocanu <mihai24vic@gmail.com>
2021-11-04 17:25:26 +0200

    added db variable for shorthand code reuse

--- TimeTrackerClient/TimeTrackerClient/Core/DataStore.swift
+++ TimeTrackerClient/TimeTrackerClient/Core/DataStore.swift
@@ -12,16 +12,17 @@ import Firebase
 class DataStore: ObservableObject {
     @Published var timeslot: String = ""
     @Published var userTimeslots = [TimeSlot]()
-    
+    let db = Firestore.firestore()
+
     func addTimeSlot(with data: [String: Any], to path: String, completion: @escaping (Error?) -> Void) {
-        Firestore.firestore().collection(path).document().setData(data) { error in
+        db.collection(path).document().setData(data) { error in
             completion(error)
         }
     }
     
     func fetchUsersTimeslots() {
         let path = "userId/YErySzP9KBgMsFw64rHrimFAUBZ2/Client x/Project x/timelLoged/02-11-2021/timeslots"
-        Firestore.firestore().collection(path).addSnapshotListener { [weak self]  (querySnapshot, error) in
+        db.collection(path).addSnapshotListener { [weak self]  (querySnapshot, error) in
             if let querySnapshot = querySnapshot{
                 self?.userTimeslots = querySnapshot.documents.compactMap { document in
                         return try? document.data(as: TimeSlot.self)
