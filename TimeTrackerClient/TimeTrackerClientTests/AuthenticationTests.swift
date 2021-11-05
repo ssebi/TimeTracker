f0ea0d50ea025bf0a3d456ec3dcd5becdc9dbf14
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-05 16:03:12 +0200

    test sign out calls authProvider's signOut method

--- TimeTrackerClient/TimeTrackerClientTests/AuthenticationTests.swift
+++ TimeTrackerClient/TimeTrackerClientTests/AuthenticationTests.swift
@@ -9,6 +9,7 @@ import XCTest
 
 protocol AuthProvider {
 	func signIn()
+	func signOut()
 }
 
 class SessionStoree {
@@ -20,6 +21,10 @@ class SessionStoree {
 
 	func signIn() {
 		authProvider.signIn()
+	}
+
+	func signOut() {
+		authProvider.signOut()
 	}
 }
 
@@ -41,12 +46,26 @@ class AuthenticationTests: XCTestCase {
 		XCTAssertEqual(spy.signInCalls, 1)
 	}
 
+	func test_signOut_callsSignOutOnAuthProvider() {
+		let spy = AuthProviderSpy()
+		let sut = SessionStoree(authProvider: spy)
+
+		sut.signOut()
+
+		XCTAssertEqual(spy.signOutCalls, 1)
+	}
+
 }
 
 private class AuthProviderSpy: AuthProvider {
 	var signInCalls = 0
+	var signOutCalls = 0
 
 	func signIn() {
 		signInCalls += 1
+	}
+
+	func signOut() {
+		signOutCalls += 1
 	}
 }
