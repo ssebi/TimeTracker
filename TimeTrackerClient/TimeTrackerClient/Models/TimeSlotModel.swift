e1c992fd189c9f361de98a6165d2c338689d62bb
Mihai Bocanu <mihai24vic@gmail.com>
2021-11-15 15:03:12 +0200

    Make Timeslot and TimeSLotDetail equatable

--- TimeTrackerClient/TimeTrackerClient/Models/TimeSlotModel.swift
+++ TimeTrackerClient/TimeTrackerClient/Models/TimeSlotModel.swift
@@ -12,12 +12,40 @@ struct TimeSlot: Identifiable, Decodable {
     @DocumentID var id: String?
     var timeSlots: TimeSlotDetail
     var total: Int
+
+    init(id: String, timeSlots: TimeSlotDetail, total: Int){
+        self.id = id
+        self.timeSlots = timeSlots
+        self.total = total
+    }
+}
+
+extension TimeSlot: Equatable {
+    static func == (lhs: TimeSlot, rhs: TimeSlot) -> Bool {
+        lhs.id == rhs.id &&
+        lhs.timeSlots == rhs.timeSlots &&
+        lhs.total == rhs.total 
+    }
 }
 
 struct TimeSlotDetail: Codable {
     var start: Date
     var end: Date
     var description: String
+
+    init(start: Date, end: Date, description: String){
+        self.start = start
+        self.end = end
+        self.description = description
+    }
+}
+
+extension TimeSlotDetail: Equatable {
+    static func == (lhs: TimeSlotDetail, rhs: TimeSlotDetail) -> Bool {
+        lhs.start == rhs.start &&
+        lhs.end == rhs.end &&
+        lhs.description == rhs.description
+    }
 }
 
 class StartEndDate: ObservableObject {
