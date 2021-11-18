7e0caef63efa7bfecc09ab9c34494d23a8c41797
Mihai Bocanu <mihai24vic@gmail.com>
2021-11-18 11:31:18 +0200

    TimeSlotPublisher add timeslot

--- TimeTrackerClient/TimeTrackerClient/Core/DataStore/Infra/FirebaseTimeslotsPublisher.swift
+++ TimeTrackerClient/TimeTrackerClient/Core/DataStore/Infra/FirebaseTimeslotsPublisher.swift
@@ -2,7 +2,24 @@
 import Firebase
 
 class FirebaseTimeslotsPublisher: TimeSlotsPublisher {
-	func addTimeSlots(timeSlot: TimeSlot, completion: @escaping TimeSlotsPublisher.Result) {
-		// TODO: - Implement
-	}
+    let db = Firestore.firestore()
+
+    func addTimeSlots(timeSlot: TimeSlot, to path: String, completion: @escaping TimeSlotsPublisher.Result) {
+        var data: [String: Any] = [:]
+        do{
+            let jsonEncoder = JSONEncoder()
+            let encodedJson = try jsonEncoder.encode(timeSlot)
+            data = try JSONSerialization.jsonObject(with: encodedJson) as! [String : Any]
+        } catch {
+            completion(.failure(error))
+        }
+
+        db.collection(path).document().setData(data) { error in
+            if error != nil {
+                completion(.failure(error!))
+                return
+            }
+            completion(.success(timeSlot))
+        }
+    }
 }
