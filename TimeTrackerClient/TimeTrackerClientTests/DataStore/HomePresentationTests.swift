51806d9e5fbf9fd18adc873bdce57e7d0b574160
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-19 19:03:19 +0200

    add home presentation tests

--- TimeTrackerClient/TimeTrackerClientTests/DataStore/HomePresentationTests.swift
+++ TimeTrackerClient/TimeTrackerClientTests/DataStore/HomePresentationTests.swift
@@ -5,8 +5,73 @@ import TimeTrackerClient
 class MockStore: TimeslotsStore {
 
 	func getTimeslots(userID: String, completion: @escaping GetTimeslotsResult) {
-		completion(.success(uniqueTimeslots))
+		completion(.success([]))
 	}
+
+}
+
+class TimeslotsLoaderSpy: TimeslotsLoader {
+	private(set) var store: TimeslotsStore
+
+	private(set) var getTimeslotsCallCount = 0
+	private var completions: [GetTimeslotsResult] = []
+
+	func getTimeslots(userID: String, completion: @escaping GetTimeslotsResult) {
+		getTimeslotsCallCount += 1
+		completions.append(completion)
+	}
+
+	init(store: TimeslotsStore) {
+		self.store = store
+	}
+
+	func completeLoadTimeslots(with timeslots: [TimeSlot], at index: Int = 0) {
+		completions[index](.success(timeslots))
+	}
+
+}
+
+class HomeScreenViewModel {
+
+	private let timeslotsLoader: TimeslotsLoader
+	@Published private(set) var timeslots: [TimeSlot] = []
+
+	init(timeslotsLoader: TimeslotsLoader) {
+		self.timeslotsLoader = timeslotsLoader
+		setup()
+	}
+
+	private func setup() {
+		timeslotsLoader.getTimeslots(userID: "YErySzP9KBgMsFw64rHrimFAUBZ2") { result in
+			self.timeslots = (try? result.get()) ?? []
+
+		}
+	}
+
+}
+
+class HomePresentationTests: XCTestCase {
+
+	func test_init_callsLoadTimeslots() {
+		let store = MockStore()
+		let loader = TimeslotsLoaderSpy(store: store)
+		let _ = HomeScreenViewModel(timeslotsLoader: loader)
+
+		XCTAssertEqual(loader.getTimeslotsCallCount, 1)
+	}
+
+	func test_setup_storesTimeslotsOnSuccess() {
+		let store = MockStore()
+		let loader = TimeslotsLoaderSpy(store: store)
+		let sut = HomeScreenViewModel(timeslotsLoader: loader)
+		let timeslots = uniqueTimeslots
+
+		loader.completeLoadTimeslots(with: timeslots)
+
+		XCTAssertEqual(sut.timeslots, timeslots)
+	}
+
+	// MARK: - Helpers
 
 	private var uniqueTimeslots: [TimeSlot] {
 		[
@@ -28,49 +93,5 @@ class MockStore: TimeslotsStore {
 						description: "some description"),
 				 total: Int.random(in: 0...100))
 	}
-
-}
-
-class TimeslotsLoaderSpy: TimeslotsLoader {
-	private(set) var store: TimeslotsStore
-
-	private(set) var getTimeslotsCallCount = 0
-
-	func getTimeslots(userID: String, completion: @escaping GetTimeslotsResult) {
-		getTimeslotsCallCount += 1
-	}
-
-	init(store: TimeslotsStore) {
-		self.store = store
-	}
-
-}
-
-class HomeScreenViewModel {
-
-	private let timeslotsLoader: TimeslotsLoader
-
-	init(timeslotsLoader: TimeslotsLoader) {
-		self.timeslotsLoader = timeslotsLoader
-		setup()
-	}
-
-	private func setup() {
-		timeslotsLoader.getTimeslots(userID: "YErySzP9KBgMsFw64rHrimFAUBZ2") { _ in }
-	}
-
-}
-
-class HomePresentationTests: XCTestCase {
-
-	func test_init_callsLoadTimeslots() {
-		let store = MockStore()
-		let loader = TimeslotsLoaderSpy(store: store)
-		let _ = HomeScreenViewModel(timeslotsLoader: loader)
-
-		XCTAssertEqual(loader.getTimeslotsCallCount, 1)
-	}
-
-	
 
 }
