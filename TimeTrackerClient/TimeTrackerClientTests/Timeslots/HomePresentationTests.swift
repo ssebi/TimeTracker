455f3e097673cfda34c502f9a09e82a3dd019197
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-22 16:24:12 +0200

    extract PresentationTests SUT creation into helper method

--- TimeTrackerClient/TimeTrackerClientTests/Timeslots/HomePresentationTests.swift
+++ TimeTrackerClient/TimeTrackerClientTests/Timeslots/HomePresentationTests.swift
@@ -5,22 +5,28 @@ import TimeTrackerClient
 class HomePresentationTests: XCTestCase {
 
 	func test_init_callsLoadTimeslots() {
-		let store = MockStore()
-		let loader = TimeslotsLoaderSpy(store: store)
-		let _ = HomeScreenViewModel(timeslotsLoader: loader, userLoader: UserLoaderMock())
+		let (_, loader) = makeSUT()
 
 		XCTAssertEqual(loader.getTimeslotsCallCount, 1)
 	}
 
 	func test_setup_storesTimeslotsOnSuccess() {
-		let store = MockStore()
-		let loader = TimeslotsLoaderSpy(store: store)
-		let sut = HomeScreenViewModel(timeslotsLoader: loader, userLoader: UserLoaderMock())
+		let (sut, loader) = makeSUT()
 		let timeslots = uniqueTimeslots
 
 		loader.completeLoadTimeslots(with: timeslots)
 
 		XCTAssertEqual(sut.timeslots, timeslots)
+	}
+
+	// MARK: - Helpers
+
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (HomeScreenViewModel, TimeslotsLoaderSpy) {
+		let store = MockStore()
+		let loader = TimeslotsLoaderSpy(store: store)
+		let sut = HomeScreenViewModel(timeslotsLoader: loader, userLoader: UserLoaderMock())
+
+		return (sut, loader)
 	}
 
 }
