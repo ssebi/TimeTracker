c228f4bddab05af14007b9cb46b0072d52df505e
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-24 17:47:09 +0200

    Add LoginViewModel

---
+++ TimeTrackerClient/TimeTrackerClient/Presentation/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  TimeTrackerClient
+//
+//  Created by Bocanu Mihai on 24.11.2021.
+//
+
+import Foundation
--- TimeTrackerClient/TimeTrackerClient/Views/Login/LoginView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/Login/LoginView.swift
@@ -14,6 +14,7 @@ struct LoginView: View {
 
 	var body: some View {
         ScrollView{
+            ProgressIndicator()
             VStack {
 			Image("timeTrackerIcon")
 				.resizable()
