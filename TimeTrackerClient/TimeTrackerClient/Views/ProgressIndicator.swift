b59f0af78c01974409c6e0e40194f995107ef38c
Mihai Bocanu <mihai24vic@gmail.com>
2021-11-24 14:45:59 +0200

    Adjust loade indicator style

--- TimeTrackerClient/TimeTrackerClient/Views/ProgressIndicator.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/ProgressIndicator.swift
@@ -11,20 +11,22 @@ struct ProgressIndicator: View {
     @State private var isCircleRotating = true
     @State private var animateStart = false
     @State private var animateEnd = true
-
+    
     var body: some View {
-
+        
         ZStack {
+            VisualEffectView(effect: UIBlurEffect(style: .light))
+                .opacity(0.9)
             Circle()
                 .stroke(lineWidth: 4)
                 .fill(Color.init(red: 0.96, green: 0.96, blue: 0.96))
-                .frame(width: 40, height: 40)
-
+                .frame(width: 35, height: 35)
+            
             Circle()
                 .trim(from: animateStart ? 1/3 : 1/9, to: animateEnd ? 2/5 : 1)
                 .stroke(lineWidth: 4)
                 .rotationEffect(.degrees(isCircleRotating ? 360 : 0))
-                .frame(width: 40, height: 40)
+                .frame(width: 35, height: 35)
                 .foregroundColor(Color.blue)
                 .onAppear() {
                     withAnimation(Animation
@@ -53,4 +55,10 @@ struct ProgressIndicator_Previews: PreviewProvider {
     static var previews: some View {
         ProgressIndicator()
     }
+}
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
 }
