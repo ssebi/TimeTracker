a558006dfc29141220e8b01eafa07110eac77de7
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-24 10:47:33 +0200

    extract sut creation into helper factory method

--- TimeTrackerClient/TimeTrackerClientTests/Clients/ClientsAPIUseCaseTests.swift
+++ TimeTrackerClient/TimeTrackerClientTests/Clients/ClientsAPIUseCaseTests.swift
@@ -1,5 +1,6 @@
 
 import XCTest
+import TimeTrackerClient
 
 class ClientsStore {
 	private(set) var getClientsCallCount = 0
@@ -24,19 +25,26 @@ class RemoteClientsLoader {
 class ClientsAPIUseCaseTests: XCTestCase {
 
 	func test_init_doesNotMessageStore() {
-		let store = ClientsStore()
-		let _ = RemoteClientsLoader(store: store)
+		let (_, store) = makeSUT()
 
 		XCTAssertEqual(store.getClientsCallCount, 0)
 	}
 
 	func test_getClients_callsStore() {
-		let store = ClientsStore()
-		let sut = RemoteClientsLoader(store: store)
+		let (sut, store) = makeSUT()
 
 		sut.getClients()
 
 		XCTAssertEqual(store.getClientsCallCount, 1)
+	}
+
+	// MARK: - Helpers
+
+	private func makeSUT() -> (RemoteClientsLoader, ClientsStore) {
+		let store = ClientsStore()
+		let sut = RemoteClientsLoader(store: store)
+
+		return (sut, store)
 	}
 
 }
