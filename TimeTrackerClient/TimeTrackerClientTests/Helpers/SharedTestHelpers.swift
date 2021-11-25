1e301117a376927b129662dc367c230d4d81aeb8
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-25 17:01:57 +0200

    Rename clinetId and project it with names

--- TimeTrackerClient/TimeTrackerClientTests/Helpers/SharedTestHelpers.swift
+++ TimeTrackerClient/TimeTrackerClientTests/Helpers/SharedTestHelpers.swift
@@ -12,8 +12,8 @@ var uniqueTimeslots: [TimeSlot] {
 var uniqueTimeslot: TimeSlot {
 	TimeSlot(id: UUID().uuidString,
 			 userId: UUID().uuidString,
-			 clientId: Int.random(in: 0...100),
-			 projectId: Int.random(in: 0...100),
+			 clientName: "Some Name",
+			 projectName: "Some Project",
 			 date: Date(),
 			 details:
 				TimeSlotDetails(
