69d1505d74e3efc40f1d9e46d3428d9b7321a5fc
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-25 12:26:42 +0200

    move TimeslotsLoaderSpy to a local scope

--- TimeTrackerClient/TimeTrackerClientTests/Timeslots/HomePresentationTests.swift
+++ TimeTrackerClient/TimeTrackerClientTests/Timeslots/HomePresentationTests.swift
@@ -33,4 +33,25 @@ class HomePresentationTests: XCTestCase {
 		return (sut, loader)
 	}
 
+	private class TimeslotsLoaderSpy: TimeslotsLoader {
+		let store: TimeslotsStore
+
+		private(set) var getTimeslotsCallCount = 0
+		private var completions: [GetTimeslotsResult] = []
+
+		func getTimeslots(userID: String, completion: @escaping GetTimeslotsResult) {
+			getTimeslotsCallCount += 1
+			completions.append(completion)
+		}
+
+		init(store: TimeslotsStore) {
+			self.store = store
+		}
+
+		func completeLoadTimeslots(with timeslots: [TimeSlot], at index: Int = 0) {
+			completions[index](.success(timeslots))
+		}
+
+	}
+
 }
