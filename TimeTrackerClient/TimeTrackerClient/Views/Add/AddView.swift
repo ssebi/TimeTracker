0a835bbde337b444473033e9f52e4daf5c5c876e
Mihai Bocanu <mihai24vic@gmail.com>
2021-11-25 15:46:48 +0200

    AddView style update

--- TimeTrackerClient/TimeTrackerClient/Views/Add/AddView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/Add/AddView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AddView: View {
+    @ObservedObject var keyboardResponder = KeyboardResponder()
     // TODO: - Move ViewModel initialization in a factory method
     @ObservedObject var timeSlotVM = TimeSlotViewModel(clientsLoader: RemoteClientsLoader(store: FirebaseClientsStore()),
                                                        timeslotPublisher: RemoteTimeSlotsPublisher(store: FirebaseTimeslotsStore()),
@@ -16,27 +17,38 @@ struct AddView: View {
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack {
-                Text(Date(), style: .date)
-                    .padding()
-                    .font(.subheadline)
-
                 if !timeSlotVM.isLoading {
                     HStack {
-                        Picker(selection: $timeSlotVM.selectedClient, label: Text("\(timeSlotVM.selectedClient)").frame(width: 100, height: 100, alignment: .center)
-                                .foregroundColor(.red)) {
+                        Picker(selection: $timeSlotVM.selectedClient,
+                               label: Text("\(timeSlotVM.clientsNames[timeSlotVM.selectedClient])")
+                        ){
                             ForEach(0 ..< timeSlotVM.clientsNames.count) { index in
                                 Text(timeSlotVM.clientsNames[index])
                             }
-                        }.pickerStyle(.menu)
+                        }
+                        .pickerStyle(.menu)
+                        .frame(width: (UIScreen.width - 55) / 2, height: 40)
+                        .background(Color.cGray)
+                        .foregroundColor(.cBlack)
+                        .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
 
-                        Picker(selection: $timeSlotVM.selectedProject, label: Text("")){
+                        Spacer()
+
+                        Picker(selection: $timeSlotVM.selectedProject,
+                               label: Text("\(timeSlotVM.projectNames[timeSlotVM.selectedProject])")
+                        ){
                             ForEach(0 ..< timeSlotVM.projectNames.count) { index in
                                 Text(timeSlotVM.projectNames[index])
                             }
                         }
                         .id(timeSlotVM.id)
-                        .pickerStyle(MenuPickerStyle())
+                        .pickerStyle(.menu)
+                        .frame(width: (UIScreen.width - 55) / 2, height: 40)
+                        .background(Color.cGray)
+                        .foregroundColor(.cBlack)
+                        .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
                     }
+                    .padding()
                 }
 
                 Spacer()
@@ -52,14 +64,17 @@ struct AddView: View {
                     Text("Task description")
                         .padding()
                     Spacer()
-                }
+                }.font(Font.custom("Avenir-Next", size: 20))
+
                 TextEditor(text: $timeSlotVM.description)
-                    .border(.gray)
+                    .border(LinearGradient.gradientButton)
                     .frame(width: UIScreen.width - 55, height: 80, alignment: .center)
 
                 Spacer()
+
                 HStack {
                     Text("\(timeSlotVM.showMessage)")
+                        .font(Font.custom("Avenir-Next", size: 20))
                     Spacer()
                 }
 
@@ -67,7 +82,12 @@ struct AddView: View {
                     addTimeSlot()
                 }
                 .buttonStyle(AddButton())
+                .navigationBarItems(trailing: Text(Date(), style: .date)
+                                        .padding()
+                                        .font(.subheadline))
             }
+            .offset(y: -keyboardResponder.currentHeight*0.5)
+
         }
         .padding()
     }
