60723dc0b36e463502c08d1b7cd3922a206ff8b1
Mihai Bocanu <mihai24vic@gmail.com>
2021-11-25 09:24:54 +0200

    Add keyboard responder :|

--- TimeTrackerClient/TimeTrackerClient/KeyboardResponder.swift
+++ TimeTrackerClient/TimeTrackerClient/KeyboardResponder.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+class KeyboardResponder: ObservableObject {
+    @Published var currentHeight: CGFloat = 0
+
+    var _center: NotificationCenter
+
+    @objc func keyBoardWillShow(notification: Notification) {
+    if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                withAnimation {
+                   currentHeight = keyboardSize.height
+                }
+            }
+        }
+    @objc func keyBoardWillHide(notification: Notification) {
+            withAnimation {
+               currentHeight = 0
+            }
+        }
+
+    init(center: NotificationCenter = .default) {
+            _center = center
+            _center.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+            _center.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+}
