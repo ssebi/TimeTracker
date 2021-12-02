375e61fbfeea25ec20c0a9fef745c27a29c3e4d6
Mihai Bocanu <mihai24vic@gmail.com>
2021-12-02 17:10:45 +0200

    Fix keyboard height

--- TimeTrackerClient/TimeTrackerClient/Views/Login/LoginView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/Login/LoginView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct LoginView: View {
     @ObservedObject private(set) var viewModel: LoginViewModel
     @ObservedObject var keyboardResponder = KeyboardResponder()
-
+    
     var body: some View {
         ZStack {
             VStack{
@@ -19,31 +19,33 @@ struct LoginView: View {
                 Rectangle()
                     .fill(Color.white)
             }.ignoresSafeArea()
-
+            
             LoginFooterView()
-
+            
             ScrollView(showsIndicators: false) {
                 VStack{
-                    Spacer()
-
-                    LogoView()
-
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                            .fill(Color.white)
-                            .frame(width: UIScreen.main.bounds.width - 65, height: (UIScreen.main.bounds.height / 2) + 70)
-                            .shadow(color: .gray, radius: 10, x: 10, y:10)
-
-                        LoginFormView(viewModel: viewModel)
+                    VStack{
+                        Spacer()
+                        
+                        LogoView()
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                .fill(Color.white)
+                                .frame(width: UIScreen.main.bounds.width - 65, height: (UIScreen.main.bounds.height / 2) + 70)
+                                .shadow(color: .gray, radius: 15, x: 10, y:5)
+                            
+                            
+                            LoginFormView(viewModel: viewModel)
+                        }
                     }
                     .offset(y: -keyboardResponder.currentHeight*0.5)
                     Spacer()
-
+                    
                 }
                 .offset(y: UIScreen.main.bounds.height / 10)
             }
-
-
+            
             if viewModel.isLoading {
                 ProgressIndicator()
             }
