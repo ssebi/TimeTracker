8782d96c5e321c92d71238418cb0f03cc9818e91
Mihai Bocanu <mihai24vic@gmail.com>
2021-12-02 15:34:34 +0200

    Separate utils from extensions

--- TimeTrackerClient/TimeTrackerClient/Core/Extensions.swift
+++ TimeTrackerClient/TimeTrackerClient/Core/Extensions.swift
@@ -14,4 +14,46 @@ extension Color {
     static let cBlue = Color("customBlue")
     static let cBlack = Color("customBlack")
     static let caribeanGreen = Color("CaribbeanGreen")
+    static let gradientTop = Color("backgroundGradientTop")
+    static let gradientBottom = Color("backgroundGradientBottom")
+}
+
+extension LinearGradient {
+    static let gradientBackground = LinearGradient(gradient: Gradient(colors: [Color.gradientTop, Color.gradientBottom]),
+                                            startPoint: .topLeading,
+                                            endPoint: .bottomTrailing)
+}
+
+extension UIScreen{
+   static let width = UIScreen.main.bounds.size.width
+   static let height = UIScreen.main.bounds.size.height
+   static let size = UIScreen.main.bounds.size
+}
+
+extension View {
+    public func gradientForeground(colors: [Color]) -> some View {
+        self.overlay(LinearGradient(gradient: .init(colors: colors),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing))
+            .mask(self)
+    }
+
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+
+    func underlineTextField() -> some View {
+            self
+                .padding(.vertical, 10)
+                .overlay(Rectangle().frame(height: 2).padding(.top, 35))
+                .foregroundColor(.cGray)
+                .padding(10)
+        }
 }
