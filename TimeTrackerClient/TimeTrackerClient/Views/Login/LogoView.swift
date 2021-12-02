c182910075b546262576833c0816e7b204fdce54
Mihai Bocanu <mihai24vic@gmail.com>
2021-12-02 15:33:51 +0200

    Update Logo View

--- TimeTrackerClient/TimeTrackerClient/Views/Login/LogoView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/Login/LogoView.swift
@@ -7,21 +7,22 @@
 
 import SwiftUI
 
-struct Logo: View {
+struct LogoView: View {
     var body: some View {
         HStack{
             Image("logo")
                 .resizable()
                 .frame(width: 50, height: 50, alignment: .center)
-            Text("TIME")
+            Text("time")
             Text("TRACKER").bold()
         }
         .foregroundColor(.white)
+        .font(Font.custom("Avenir-Light", size: 35.0))
     }
 }
 
 struct Logo_Previews: PreviewProvider {
     static var previews: some View {
-        Logo()
+        LogoView()
     }
 }
