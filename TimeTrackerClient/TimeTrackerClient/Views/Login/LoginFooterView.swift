f56445d98a4ee8d403084f00de813637cda14e8f
Mihai Bocanu <mihai24vic@gmail.com>
2021-12-03 13:03:14 +0200

    Add coming soon alert

--- TimeTrackerClient/TimeTrackerClient/Views/Login/LoginFooterView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/Login/LoginFooterView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct LoginFooterView: View {
+    @State var showAlert = false
+
     var body: some View {
-        VStack{
+        VStack {
             Spacer()
-            Text("Forgot password?")
-                .padding()
-            Text("Register a new account")
+            Button("Forgot password?") {
+                showAlert = true
+                    }
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("Comming soon"), message: Text("This functionality is under development"), dismissButton: .default(Text("Got it!")))
+                    }
+                    .padding()
+            Button("Create a new account") {
+                showAlert = true
+                    }
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("Comming soon"), message: Text("This functionality is under development"), dismissButton: .default(Text("Got it!")))
+                    }
         }
         .padding()
         .foregroundColor(.cGray)
