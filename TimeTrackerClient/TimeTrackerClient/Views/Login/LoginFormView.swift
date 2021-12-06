c2ee64383965a95109373a877c018cd358dae135
Mihai Bocanu <mihai24vic@gmail.com>
2021-12-06 14:30:31 +0200

    Add keyboard custom return

--- TimeTrackerClient/TimeTrackerClient/Views/Login/LoginFormView.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/Login/LoginFormView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LoginFormView: View {
     @ObservedObject private(set) var viewModel: LoginViewModel
     @State private var isSecured: Bool = true
+    @State var focused: [Bool] = [true, false]
 
     var body: some View {
         VStack{
@@ -28,12 +29,17 @@ struct LoginFormView: View {
                     .foregroundColor(.red)
                     .font(Font.custom("Avenir-Light", size: 20))
                     .padding()
+
                 HStack {
                     Image(systemName: "person.fill")
-                    TextField("", text: $viewModel.username)
-                        .placeholder(when: viewModel.username.isEmpty) {
-                            Text("E-mail").foregroundColor(.cGray)
-                        }
+                    CustomTextField(
+                        keyboardType: UIKeyboardType.default,
+                        returnVal: UIReturnKeyType.next,
+                        tag: 0,
+                        text: $viewModel.username,
+                        isfocusAble: self.$focused,
+                        placeholder: "E-mail"
+                    )
                         .cornerRadius(10)
                         .foregroundColor(.cBlack)
                         .autocapitalization(.none)
@@ -50,37 +56,32 @@ struct LoginFormView: View {
                     Image(systemName: "lock.fill")
 
                     ZStack(alignment: .trailing) {
-                                if isSecured {
-                                    SecureField("", text: $viewModel.password)
-                                        .placeholder(when: viewModel.password.isEmpty) {
-                                            Text("Password").foregroundColor(.cGray)
-                                        }
+                                    CustomTextField(
+                                        keyboardType: UIKeyboardType.default,
+                                        returnVal: UIReturnKeyType.done,
+                                        tag: 1,
+                                        text: $viewModel.password,
+                                        isfocusAble: self.$focused,
+                                        isSecured: true,
+                                        placeholder: "Password"
+                                    )
                                         .foregroundColor(.cBlack)
                                         .cornerRadius(10)
                                         .onTapGesture {
                                             viewModel.errrorMessage = ""
                                         }
-                                } else {
-                                    TextField("", text: $viewModel.password)
-                                        .placeholder(when: viewModel.password.isEmpty) {
-                                            Text("Password").foregroundColor(.cGray)
-                                        }
-                                        .foregroundColor(.cBlack)
-                                        .cornerRadius(10)
-                                        .onTapGesture {
-                                            viewModel.errrorMessage = ""
-                                        }
-                                }
-                                Button(action: {
-                                    isSecured.toggle()
-                                }) {
-                                    Image(systemName: self.isSecured ? "eye.slash" : "eye")
-                                        .accentColor(.cGray)
-                                }
+
+//                                Button(action: {
+//                                    isSecured.toggle()
+//                                }) {
+//                                    Image(systemName: self.isSecured ? "eye.slash" : "eye")
+//                                        .accentColor(.cGray)
+//                                }
                             }
                 }.underlineTextField()
             }
-            .padding(EdgeInsets(top: 1, leading: 45, bottom: 1, trailing: 45))
+            .padding(EdgeInsets(top: 10, leading: 45, bottom: 10, trailing: 45))
+            .frame(width: .infinity, height: 50, alignment: .center)
 
             Section {
                 Button(action: {
@@ -105,5 +106,63 @@ struct LoginFormView: View {
 struct LoginFormView_Previews: PreviewProvider {
     static var previews: some View {
         LoginFormView(viewModel: LoginViewModel(session: SessionStore(authProvider: FirebaseAuthProvider())))
+    }
+}
+
+struct CustomTextField: UIViewRepresentable {
+    let keyboardType: UIKeyboardType
+    let returnVal: UIReturnKeyType
+    let tag: Int
+    @Binding var text: String
+    @Binding var isfocusAble: [Bool]
+    var isSecured: Bool = false
+    var placeholder: String
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField(frame: .zero)
+        textField.keyboardType = self.keyboardType
+        textField.returnKeyType = self.returnVal
+        textField.tag = self.tag
+        textField.delegate = context.coordinator
+        textField.autocorrectionType = .no
+        textField.isSecureTextEntry = isSecured
+        textField.placeholder = placeholder
+
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        if isfocusAble[tag] {
+            uiView.becomeFirstResponder()
+        } else {
+            uiView.resignFirstResponder()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: CustomTextField
+
+        init(_ textField: CustomTextField) {
+            self.parent = textField
+        }
+
+        func updatefocus(textfield: UITextField) {
+            textfield.becomeFirstResponder()
+        }
+
+func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            if parent.tag == 0 {
+                parent.isfocusAble = [false, true]
+                parent.text = textField.text ?? ""
+            } else if parent.tag == 1 {
+                parent.isfocusAble = [false, false]
+                parent.text = textField.text ?? ""
+         }
+        return true
+        }
     }
 }
