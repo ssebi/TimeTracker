e2bfdb49c6f46ede865c70e05b2470c6c7fe4b36
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-12-08 11:49:43 +0200

    separate dependencies in their separate class

--- TimeTrackerClient/TimeTrackerClient/TimeTrackerClientApp.swift
+++ TimeTrackerClient/TimeTrackerClient/TimeTrackerClientApp.swift
@@ -14,32 +14,14 @@ struct TimeTrackerClientApp: App {
 	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 	@StateObject var sessionStore = SessionStore(authProvider: FirebaseAuthProvider())
 
-	private var clientsStore: ClientsStore = {
-		FirebaseClientsStore()
-	}()
-	private var timeslotsStore: TimeslotsStore = {
-		FirebaseTimeslotsStore()
-	}()
-	private var userLoader: FirebaseUserLoader = {
-		FirebaseUserLoader()
-	}()
-	
-	private var remoteTimeslotsLoader: TimeslotsLoader {
-		RemoteTimeslotsLoader(store: timeslotsStore)
-	}
-	private var remoteClientsLoader: ClientsLoader {
-		RemoteClientsLoader(store: clientsStore)
-	}
-	private var remoteTimeslotsPublisher: TimeSlotsPublisher {
-		RemoteTimeSlotsPublisher(store: timeslotsStore)
-	}
+	private let dependencies = Dependencies()
 	
 	var body: some Scene {
 		WindowGroup {
 			Group {
 				if sessionStore.user != nil {
-					HomeScreenUIComposer.makeHomeScreen(timeslotsLoader: remoteTimeslotsLoader, userLoader: userLoader, addView: {
-						AddScreenUIComposer.makeAddScreen(clientsLoader: remoteClientsLoader, timeslotsPublisher: remoteTimeslotsPublisher, userLoader: userLoader)
+					HomeScreenUIComposer.makeHomeScreen(timeslotsLoader: dependencies.remoteTimeslotsLoader, userLoader: dependencies.userLoader, addView: {
+						dependencies.addScreen
 					})
 				} else {
                     LoginView(viewModel: LoginViewModel(session: sessionStore))
@@ -56,4 +38,34 @@ struct TimeTrackerClientApp: App {
 		}
 	}
 
+}
+
+// MARK: - Dependencies
+
+extension TimeTrackerClientApp {
+	fileprivate class Dependencies {
+		fileprivate var clientsStore: ClientsStore = {
+			FirebaseClientsStore()
+		}()
+		fileprivate var timeslotsStore: TimeslotsStore = {
+			FirebaseTimeslotsStore()
+		}()
+		fileprivate var userLoader: FirebaseUserLoader = {
+			FirebaseUserLoader()
+		}()
+
+		fileprivate lazy var addScreen: AddView = {
+			AddScreenUIComposer.makeAddScreen(clientsLoader: remoteClientsLoader, timeslotsPublisher: remoteTimeslotsPublisher, userLoader: userLoader)
+		}()
+
+		fileprivate var remoteTimeslotsLoader: TimeslotsLoader {
+			RemoteTimeslotsLoader(store: timeslotsStore)
+		}
+		fileprivate var remoteClientsLoader: ClientsLoader {
+			RemoteClientsLoader(store: clientsStore)
+		}
+		fileprivate var remoteTimeslotsPublisher: TimeSlotsPublisher {
+			RemoteTimeSlotsPublisher(store: timeslotsStore)
+		}
+	}
 }
