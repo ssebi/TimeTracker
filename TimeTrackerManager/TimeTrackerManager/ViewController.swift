9d177d0df75d4b43dbcb5f43050e501908d2c04f
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-12-09 18:02:01 +0200

    create Manager UIKit app

---
+++ TimeTrackerManager/TimeTrackerManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeTrackerManager
+//
+//  Created by Sebastian Vidrea on 09.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
