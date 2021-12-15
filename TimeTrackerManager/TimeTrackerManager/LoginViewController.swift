72dfcb07deff051c8f151010c2d44d5f758cc035
Mihai Bocanu <mihai24vic@gmail.com>
2021-12-15 15:14:30 +0200

    Rename LoginViewController

--- TimeTrackerManager/TimeTrackerManager/LoginViewController.swift
+++ TimeTrackerManager/TimeTrackerManager/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
