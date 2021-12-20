c8cffab30724398e1c3caf6bdb179a514f49117c
Mihai Bocanu <mihai24vic@gmail.com>
2021-12-20 16:45:31 +0200

    Add user table cell list

---
+++ TimeTrackerManager/TimeTrackerManager/User.swift
@@ -0,0 +1,12 @@
+//
+//  UserModel.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 20.12.2021.
+//
+
+import Foundation
+
+struct User {
+
+}
---
+++ TimeTrackerManager/TimeTrackerManager/UserListCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserListCell.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 20.12.2021.
+//
+
+import UIKit
+
+class UserListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
