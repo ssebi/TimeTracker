791835d4249aab34cd9e7e1fc4e92c9929afeff8
Mihai Bocanu <mihai24vic@gmail.com>
2022-01-12 15:53:12 +0200

    Remove commented code

--- TimeTrackerManager/TimeTrackerManager/Core/Feature/UserLoader.swift
+++ TimeTrackerManager/TimeTrackerManager/Core/Feature/UserLoader.swift
@@ -1,13 +1,4 @@
 
 public protocol UserLoader {
-
-//    struct UndefinedError: Error {}
-//
-//    typealias GetUsersResult = (Result<[User], Error>) -> Void
-//
-//    typealias GetUserInfoResult = (Result<Void, Error>) -> Void
-//
-//    func getUsers(completion: @escaping GetUsersResult)
-
 }
 
