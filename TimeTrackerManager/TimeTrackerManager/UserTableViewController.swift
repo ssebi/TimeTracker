54700dd4a4f37235b4ed46fa3e2730a230f9a8bb
Mihai Bocanu <mihai24vic@gmail.com>
2022-01-14 13:18:57 +0200

    Reaload table on users set

--- TimeTrackerManager/TimeTrackerManager/UserTableViewController.swift
+++ TimeTrackerManager/TimeTrackerManager/UserTableViewController.swift
@@ -10,31 +10,36 @@ import UIKit
 class UserTableViewController: UITableViewController {
 
     var userLoader = FirebaseUserLoader()
-    var user: [User] = []
+    var users: [User] = [] {
+        didSet { tableView.reloadData() }
+    }
     // Data
     @IBOutlet var UserTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadUserData()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     // MARK: - Table view data source
+    func loadUserData() {
+        userLoader.getUsers() { result in
+            if let user = try? result.get() {
+                self.users = user
+            }
+        }
+    }
 }
 
 extension UserTableViewController {
     static let usersCellIdentifier = "UserListCell"
 
-
+    //TODO: use new api for tableview
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        userLoader.getUsers() { result in
-            if let user = try? result.get() {
-                self.user = user
-            }
-        }
-        return user.count
+        users.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,7 +47,8 @@ extension UserTableViewController {
             fatalError("Unable to deque UserCell")
         }
 
-        cell.userName.text = user[indexPath.row].firstName
+        let userCell = users[indexPath.row]
+        cell.userName.text = userCell.firstName
         cell.userProfilePicture.image = UIImage(systemName: "person.fill.viewfinder")
 
         return cell
