80c69dabced54f0976d2a05df6910ae0a73803f7
Mihai Bocanu <mihai24vic@gmail.com>
2022-01-25 17:36:05 +0200

    Rearange file structures into folders

---
+++ TimeTrackerManager/TimeTrackerManager/Core/Controller/Projects/AddProjectsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddProjectsViewController.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 25.01.2022.
+//
+
+import UIKit
+
+class AddProjectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ TimeTrackerManager/TimeTrackerManager/Core/Controller/Projects/ProjectsTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectsTableViewController.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 25.01.2022.
+//
+
+import UIKit
+
+class ProjectsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
