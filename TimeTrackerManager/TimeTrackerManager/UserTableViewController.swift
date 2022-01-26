07c63fe38a8914b4b0d964e62a48ff714fc8b3e0
Vidrea Sebastian <vidreasebastian@gmail.com>
2022-01-26 11:44:20 +0200

    make navigation bar tint color white for better contrast

--- TimeTrackerManager/TimeTrackerManager/UserTableViewController.swift
+++ TimeTrackerManager/TimeTrackerManager/UserTableViewController.swift
@@ -19,6 +19,7 @@ class UserTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		navigationController?.navigationBar.tintColor = .white
         loadUserData()
     }
 
