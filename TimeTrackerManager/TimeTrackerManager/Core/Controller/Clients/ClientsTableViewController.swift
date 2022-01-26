8c0a7def462faa36f17c76af295c5dca9f4676ca
Mihai Bocanu <mihai24vic@gmail.com>
2022-01-26 18:19:12 +0200

    Update clientTableView with proper values

--- TimeTrackerManager/TimeTrackerManager/Core/Controller/Clients/ClientsTableViewController.swift
+++ TimeTrackerManager/TimeTrackerManager/Core/Controller/Clients/ClientsTableViewController.swift
@@ -28,56 +28,38 @@ class ClientsTableViewController: UITableViewController {
             }
         }
     }
+}
 
-    // MARK: - Table view data source
+extension ClientsTableViewController {
     static let clientCellIdentifier = "ClientListCell"
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        clients.count
+    }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.clientCellIdentifier, for: indexPath) as? ClientListCell else {
-            fatalError("Unable to deque UserCell")
+            fatalError("Unable to deque client Cell")
         }
 
-        let userCell = users[indexPath.row]
+        //temporary leave it like this
+        let clientRandomAvatarUrl = URL(string: "https://avatars.dicebear.com/api/miniavs/client.png")
+
+        let clientCell = clients[indexPath.row]
         DispatchQueue.global().async {
-            if let data = try? Data(contentsOf: userCell.profilePictureURLOrDefault),
+            if let data = try? Data(contentsOf: clientRandomAvatarUrl! ),
                let image = UIImage(data: data) {
                 DispatchQueue.main.async {
-                    cell.userProfilePicture.image = image
+                    cell.clientsAvatar.image = image
                 }
             } else {
-                cell.userProfilePicture.image = UIImage(systemName: "xmark.icloud")!
+                cell.clientsAvatar.image = UIImage(systemName: "xmark.icloud")!
             }
         }
 
-        cell.userName.text = userCell.name
-        cell.hourRate.text = userCell.hourRate
-
-        DispatchQueue.global().async {
-            userCell.timeSlots?(userCell.userId) { result in
-                var totalHours = 0
-                var allProjects = Set<String>()
-
-                if let success = try? result.get() {
-                    success.forEach { timeSlot in
-                        totalHours += timeSlot.total
-                        allProjects.insert(timeSlot.projectName)
-                    }
-                    DispatchQueue.main.async {
-                        cell.totalHours.text = "\(totalHours)"
-                        cell.userProjects.text = "\(allProjects)"
-                    }
-                }
-            }
-        }
+        cell.clientName.text = clientCell.name
+        cell.clientsProject.text = "\(clientCell.projects)"
 
         return cell
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        let clientCell = clients[indexPath.row]
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        present(UserDetailViewController(userDetail: clients[indexPath.row]), animated: true)
     }
 }
