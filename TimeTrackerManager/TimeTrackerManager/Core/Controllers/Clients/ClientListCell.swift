37cef355d48d4fe47681fe49704e73fa28ec6120
Mihai Bocanu <mihai24vic@gmail.com>
2022-01-27 18:16:59 +0200

    Remove unuse functions

--- TimeTrackerManager/TimeTrackerManager/Core/Controllers/Clients/ClientListCell.swift
+++ TimeTrackerManager/TimeTrackerManager/Core/Controllers/Clients/ClientListCell.swift
@@ -11,15 +11,4 @@ class ClientListCell: UITableViewCell {
     @IBOutlet var clientsAvatar: UIImageView!
     @IBOutlet var clientName: UILabel!
     @IBOutlet var clientsProject: UILabel!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 }
