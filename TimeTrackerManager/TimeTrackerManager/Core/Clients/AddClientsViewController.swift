a2c0170c67af741128cd7cf77dc0be5f25e03226
Mihai Bocanu <mihai24vic@gmail.com>
2022-01-27 16:22:52 +0200

    Added Clients tableView Controller

---
+++ TimeTrackerManager/TimeTrackerManager/Core/Clients/AddClientsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AddProjectsViewController.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 25.01.2022.
+//
+
+import UIKit
+
+class AddClientsViewController: UIViewController {
+
+    @IBOutlet var clientName: UITextField!
+    @IBOutlet var clientProject: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    let clientPublisher = FirebaseClientPublisher()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.isHidden = true
+
+    }
+    @IBAction func addClientButtonPressed(_ sender: Any) {
+        clientPublisher.createClient(clientName.text ?? "", project: clientProject.text ?? "") { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+                case .success:
+                    self.validationError(title: "Success", message: "Client Created", hasError: false)
+                case .failure(let error):
+                    self.validationError(title: "Error", message: "Something went wrong: \(error)", hasError: true)
+            }
+
+        }
+    }
+
+    func toggleSpiner(isHidden: Bool) {
+        activityIndicator.isHidden = isHidden
+        if isHidden == true {
+            activityIndicator.stopAnimating()
+        } else {
+            activityIndicator.startAnimating()
+        }
+    }
+}
+
+extension AddClientsViewController {
+    func validationError(title: String, message: String, hasError: Bool) {
+        self.toggleSpiner(isHidden: true)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { action in
+
+            if !hasError { self.dismisView() }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func dismisView(){
+        self.navigationController?.popViewController(animated: true)
+    }
+}
---
+++ TimeTrackerManager/TimeTrackerManager/Core/Clients/ClientsTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ProjectsTableViewController.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 25.01.2022.
+//
+
+import UIKit
+import TimeTrackerCore
+
+class ClientsTableViewController: UITableViewController {
+
+    var clients: [Client] = [] {
+        didSet { tableView.reloadData() }
+    }
+
+    let clientsLoader = FirebaseClientsLoader(store: FirebaseClientsStore())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadClientsData()
+    }
+
+    func loadClientsData() {
+        clientsLoader.getClients() { result in
+            if let clients = try? result.get() {
+                self.clients = clients
+            }
+        }
+    }
+}
+
+extension ClientsTableViewController {
+    static let clientCellIdentifier = "ClientListCell"
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        clients.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.clientCellIdentifier, for: indexPath) as? ClientListCell else {
+            fatalError("Unable to deque client Cell")
+        }
+
+        //temporary leave it like this
+        let clientRandomAvatarUrl = URL(string: "https://avatars.dicebear.com/api/miniavs/client.png")
+
+        let clientCell = clients[indexPath.row]
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: clientRandomAvatarUrl! ),
+               let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    cell.clientsAvatar.image = image
+                }
+            } else {
+                cell.clientsAvatar.image = UIImage(systemName: "xmark.icloud")!
+            }
+        }
+
+        cell.clientName.text = clientCell.name
+        cell.clientsProject.text = "\(clientCell.projects)"
+
+        return cell
+    }
+}
