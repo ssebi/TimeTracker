18380f5f62560ce0a19637f15d0681baaf6401d2
Mihai Bocanu <mihai24vic@gmail.com>
2022-02-04 22:26:15 +0200

    Populate invoice atempt

--- TimeTrackerManager/TimeTrackerManager/Core/Controllers/Clients/ClientDetailViewController.swift
+++ TimeTrackerManager/TimeTrackerManager/Core/Controllers/Clients/ClientDetailViewController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 
-final class ClientsDetailViewController: UIViewController {
+final class ClientDetailViewController: UIViewController {
+
     @IBOutlet private var clientNAme: UITextView!
+    var invoice = Invoice(client: "Client Name", invoiceNumber: "TMTRK001", product: "Programing hours")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        if !clientDetail.isEmpty {
+//            clientNAme.text = clientDetail
+//        }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
       if segue.identifier == "previewInvoice" {
         guard let invoiceVC = segue.destination as? InvoicePreviewViewController else { return }
         let pdfCreator = PDFCreator()
-          invoiceVC.documentData = pdfCreator.createInvoice()
+          invoiceVC.documentData = pdfCreator.createInvoice(invoice: invoice)
       }
     }
 }
