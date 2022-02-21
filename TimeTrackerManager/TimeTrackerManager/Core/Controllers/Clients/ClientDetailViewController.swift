5438f4a8b7ebdc9d1330a6667e52b99c1ef95771
Mihai Bocanu <mihai24vic@gmail.com>
2022-02-21 20:23:19 +0200

    Add share function

--- TimeTrackerManager/TimeTrackerManager/Core/Controllers/Clients/ClientDetailViewController.swift
+++ TimeTrackerManager/TimeTrackerManager/Core/Controllers/Clients/ClientDetailViewController.swift
@@ -14,6 +14,10 @@ final class ClientDetailViewController: UIViewController {
     private var invoiceManager = FirebaseInvoiceManager()
     private var invoiceNo: InvoiceNo?
     private let dateFormatter = DateFormatter()
+    let logo = UIImage(named: "parhelion_logo_light")
+    var clientInvoiceDetail: ClientBillingInfo?
+    var invoice: Invoice?
+    typealias InvoiceResult = (Result<InvoiceNo, Error>) -> Void
 
     @IBOutlet weak var clientName: UILabel!
     @IBOutlet private var invoiceTotal: UILabel!
@@ -34,16 +38,55 @@ final class ClientDetailViewController: UIViewController {
         super.viewDidLoad()
 
         datePicker.datePickerMode = UIDatePicker.Mode.date
-		datePicker.addTarget(self, action: #selector(onDateChanged), for: .valueChanged)
+        datePicker.addTarget(self, action: #selector(onDateChanged), for: .valueChanged)
         clientName.text = client?.name
-        loadInvoiceNo()
+
+        loadInvoiceNo { [weak self] result in
+            if case let .success(_) = result {
+                self?.invoice = Invoice(
+                    client: (self?.client?.name ?? "Unamed"),
+                    invoiceNumber: "\(self?.invoiceNo!.series)\(self?.invoiceNoTextField.text!.description)",
+                    product: "Software development services",
+                    quantity: Int(self?.invoiceTotal.text ?? "") ?? 0,
+                    unitCost: self?.client?.hourRate ?? 0,
+                    invoiceDate: Date().stringToday()
+                )
+            }
+        }
+
+        loadInvoiceTotal()
+
+        clientInvoiceDetail = ClientBillingInfo(
+            name: client?.name ?? "Unamed",
+            vat: "VAT: \(client?.vat ?? "VAT:")",
+            address: "Address: \(client?.address ?? "")",
+            country: "Country: \(client?.country ?? "")"
+        )
+    }
+
+    @objc private func onDateChanged() {
         loadInvoiceTotal()
     }
 
-	@objc private func onDateChanged() {
-		loadInvoiceTotal()
-	}
+    @IBAction func shareAction(_ sender: Any) {
+        guard let image = logo,
+              let clientInvoice = clientInvoiceDetail,
+              let invoice = invoice else { return }
 
+        let invoiceRef = InvoiceCreator(
+            title: "Invoice",
+            image: image,
+            clientDetail: clientInvoice
+        )
+        
+        invoiceRef.createInvoice(invoice: invoice)
+
+        let vc = UIActivityViewController(
+            activityItems: [invoiceRef],
+            applicationActivities: []
+        )
+        present(vc, animated: true, completion: nil)
+    }
     @IBAction private func previewInvoiceButton(_ sender: Any) {
         if invoiceNoTextField.text != nil,
            invoiceNo?.no != nil,
@@ -55,12 +98,13 @@ final class ClientDetailViewController: UIViewController {
         }
     }
 
-    private func loadInvoiceNo() {
+    private func loadInvoiceNo(completion: @escaping InvoiceResult) {
         invoiceManager.getInvoiceNo { [weak self] result in
             if let invoiceNo = try? result.get() {
                 self?.invoiceNo = invoiceNo
                 self?.invoiceNoTextField.text = "\(invoiceNo.no)"
                 self?.invoiceSeriesLabel.text = "\(invoiceNo.series)"
+                completion(result)
             }
         }
     }
@@ -75,35 +119,19 @@ final class ClientDetailViewController: UIViewController {
         }
     }
 
-    // do this on button press
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "previewInvoice",
+              let invoiceVC = segue.destination as? InvoicePreviewViewController,
+              let image = logo,
+              let clientDetail = clientInvoiceDetail,
+              let invoice = invoice else { return }
 
-		let clientInvoiceDetail = ClientBillingInfo(
-            name: client?.name ?? "Unamed",
-            vat: "VAT: \(client?.vat ?? "VAT:")",
-            address: "Address: \(client?.address ?? "")",
-            country: "Country: \(client?.country ?? "")"
-		)
+        let pdfCreator = InvoiceCreator(
+            title: "Invoice",
+            image: image,
+            clientDetail: clientDetail
+        )
 
-		let invoice = Invoice(
-			client: (client?.name ?? "Unamed"),
-            invoiceNumber: "\(invoiceNo!.series)\(invoiceNoTextField.text!.description)",
-			product: "Software development services",
-            quantity: Int(invoiceTotal.text ?? "") ?? 0,
-            unitCost: client?.hourRate ?? 0,
-            invoiceDate: Date().stringToday()
-		)
-
-		guard segue.identifier == "previewInvoice",
-			  let invoiceVC = segue.destination as? InvoicePreviewViewController,
-			  let image = UIImage(named: "parhelion_logo_light") else { return }
-
-		let pdfCreator = InvoiceCreator(
-			title: "Invoice",
-			image: image,
-			clientDetail: clientInvoiceDetail
-		)
-
-		invoiceVC.documentData = pdfCreator.createInvoice(invoice: invoice)
-	}
+        invoiceVC.documentData = pdfCreator.createInvoice(invoice: invoice)
+    }
 }
