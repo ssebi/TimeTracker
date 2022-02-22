4176d82df0d6b6448d22013dc9cb866019e7692b
Mihai Bocanu <mihai24vic@gmail.com>
2022-02-22 22:14:47 +0200

    Update invoice view

---
+++ TimeTrackerManager/TimeTrackerManager/Core/Controllers/InvoiceListCell.swift
@@ -0,0 +1,14 @@
+//
+//  InvoiceListCell.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 22.02.2022.
+//
+
+import Foundation
+import UIKit
+
+final class InvoiceListCell: UITableViewCell {
+    @IBOutlet var pdfIcon: UIImageView!
+    @IBOutlet var pdfTitle: UILabel!
+}
