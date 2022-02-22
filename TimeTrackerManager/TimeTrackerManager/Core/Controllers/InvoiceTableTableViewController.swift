cf5b058e7598978e3d0e1db8b673defdffc1a518
Mihai Bocanu <mihai24vic@gmail.com>
2022-02-22 22:14:47 +0200

    Update invoice view

---
+++ TimeTrackerManager/TimeTrackerManager/Core/Controllers/InvoiceTableTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InvoiceTableTableViewController.swift
+//  TimeTrackerManager
+//
+//  Created by Bocanu Mihai on 22.02.2022.
+//
+
+import UIKit
+
+class InvoiceTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
