60594f48326f6c0e36781b5ec56b358634011515
Mihai Bocanu <mihai24vic@gmail.com>
2022-03-02 16:45:45 +0200

    Fix braking changes

--- TimeTrackerClient/TimeTrackerClient/Views/Login/SignUp.swift
+++ TimeTrackerClient/TimeTrackerClient/Views/Login/SignUp.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import TimeTrackerAuth
 
 struct SignUp: View {
+    @ObservedObject private(set) var viewModel: LoginViewModel
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,6 +18,6 @@ struct SignUp: View {
 
 struct SignUp_Previews: PreviewProvider {
     static var previews: some View {
-        SignUp()
+        SignUp(viewModel: LoginViewModel(session: SessionStore(authProvider: FirebaseAuthProvider())))
     }
 }
